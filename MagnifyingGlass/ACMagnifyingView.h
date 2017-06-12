cdc26450156105b003a43329e890c52e47692963
ayong <2238899650@qq.com>
2017-06-12 19:42:57 +0800

    Change .gitignore

---
+++ MagnifyingGlass/ACMagnifyingView.h
@@ -0,0 +1,19 @@
+//
+//  ACMagnifyingView.h
+//  MagnifyingGlass
+//
+//  Created by Arnaud Coomans on 30/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ACMagnifyingGlass;
+
+@interface ACMagnifyingView : UIView
+
+@property (nonatomic, retain) ACMagnifyingGlass *magnifyingGlass;
+@property (nonatomic, assign) CGFloat magnifyingGlassShowDelay;
+@property (unsafe_unretained, nonatomic) IBOutlet UIView *view;
+
+@end
---
+++ MagnifyingGlassDemo/ACViewController.h
@@ -0,0 +1,21 @@
+//
+//  ACViewController.h
+//  MagnifyingGlass
+//
+//  Created by Arnaud Coomans on 30/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ACMagnifyingView;
+
+@interface ACViewController : UIViewController
+//@property (nonatomic, retain) IBOutlet ACMagnifyingView *magnifyingView;
+
+@property (nonatomic, strong) ACMagnifyingView *magnifyingView;
+@property (unsafe_unretained, nonatomic) IBOutlet UILabel *textLab;
+
+
+
+@end
